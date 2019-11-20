ba030e39388eec75e5d705c7c372c173f6cf5f44
Gabriel <gaapalhares@gmail.com>
2019-11-19 21:23:59 -0300

    Initial Commit

---
+++ ToDoAPI/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ToDoAPI.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
